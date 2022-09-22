b4be54024044eb75e504556387562b4f2ee60a44
Samanta Fluture <samantafluture@gmail.com>
2022-09-22 14:03:37 -0400

    add more styles to the info container at home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,10 @@
-import { HomeContainer, InfoContainer,InfoContainerBackground, InfoText  } from './styles'
+import {
+	HomeContainer,
+	InfoContainer,
+	InfoContainerBackground,
+	InfoText,
+	InfoItems,
+} from './styles'
 import { Timer, ShoppingCart, Package, Coffee } from 'phosphor-react'
 import introImage from '../../assets/coffee-cup-and-beans.png'
 
@@ -9,35 +15,41 @@ export function Home() {
 				<InfoContainerBackground>
 					<InfoContainer>
 						<InfoText>
-							<h1>Find the perfect coffee for any time of day</h1>
+							<h1>
+								Find the perfect coffee for any time of the day
+							</h1>
 							<h2>
 								With Coffee Delivery you get your coffee
 								wherever you are, anytime
 							</h2>
-							<div>
-								<div>
+							<InfoItems>
+								<article>
 									<div>
 										<ShoppingCart size={16} weight='fill' />
-										<span>Simple and secure purchase</span>
 									</div>
+									<span>Simple and secure purchase</span>
+								</article>
+								<article>
 									<div>
 										<Timer size={16} weight='fill' />
-										<span>Fast and tracked delivery</span>
 									</div>
-								</div>
-								<div>
+									<span>Fast and tracked delivery</span>
+								</article>
+								<article>
 									<div>
 										<Package size={16} weight='fill' />
-										<span>
-											Packaging keeps the coffee intact
-										</span>
 									</div>
+									<span>
+										Packaging keeps the coffee intact
+									</span>
+								</article>
+								<article>
 									<div>
 										<Coffee size={16} weight='fill' />
-										<span>Coffee arrives fresh to you</span>
 									</div>
-								</div>
-							</div>
+									<span>Coffee arrives fresh to you</span>
+								</article>
+							</InfoItems>
 						</InfoText>
 						<img src={introImage} alt='coffee cup and beans' />
 					</InfoContainer>
